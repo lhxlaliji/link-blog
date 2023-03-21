931b011bad39a28d25347955663fbbb9a6aae130
lhxlaliji <lin909062828@gmail.com>
2023-03-21 20:15:43 +0800

    first-build

---
+++ fontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style  scoped>
+  .container{
+    width: 50px;
+    height: 50px;
+    background-color: red;
+  }
+</style>
---
+++ fontend/vite.config.js
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
